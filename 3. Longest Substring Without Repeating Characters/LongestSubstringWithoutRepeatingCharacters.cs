f8775fc2fb23d03d689a64bc26c397331cef96ee
fuookami <fuookami@163.com>
2019-01-27 00:15:07 +0800

    优化第3题的实现

--- 3. Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters.cs
+++ 3. Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters.cs
@@ -3,27 +3,30 @@ using System.Linq;
 
 public class Solution {
     public int LengthOfLongestSubstring(string s) {
-        List<char> sub = new List<char>();
-        int maxLen = 0;
-        foreach (var ch in s) {
-            int index = sub.IndexOf(ch);
-            if (index != -1) {
-                if (sub.Count > maxLen) {
-                    maxLen = sub.Count;
-                }
-                sub.RemoveRange(0, index + 1);
-            }
-            sub.Add(ch);
+        var previous = 0;
+        var maxLen = 0;
+        var thisLen = 0;
+        var indexs = new int[128];
+        for (int i = 0; i != 128; ++i) {
+            indexs[i] = -1;
         }
-        if (sub.Count > maxLen) {
-            maxLen = sub.Count;
+
+        for (int i = 0; i != s.Length; ++i) {
+            var ch = (int)s[i];
+            var index = indexs[ch];
+            if (index >= previous) {
+                thisLen = i - previous;
+                if (thisLen > maxLen) {
+                    maxLen = thisLen;
+                }
+                previous = index + 1;
+            }
+            indexs[ch] = i;
+        }
+        thisLen = s.Length - previous;
+        if (thisLen > maxLen) {
+            maxLen = thisLen;
         }
         return maxLen;
-    }
-}
-
-public class Solution {
-    public int LengthOfLongestSubstring(string s) {
-        
     }
 }
